070b0e7eba5808c866c792bcc93d22b441238bf4
Rob Catling <github@rpc-soft.com>
2020-05-04 14:07:39 +0100

    CR-907 demo data

--- src/main/java/uk/gov/ons/ctp/integration/mockcaseapiservice/endpoint/CaseServiceMockStub.java
+++ src/main/java/uk/gov/ons/ctp/integration/mockcaseapiservice/endpoint/CaseServiceMockStub.java
@@ -139,7 +139,7 @@ public final class CaseServiceMockStub implements CTPEndpoint {
     newQuestionnaire.setQuestionnaireId(
         String.format("%010d", new Random().nextInt(Integer.MAX_VALUE)));
     newQuestionnaire.setUac("bk5pkrx5hscrclb7");
-    newQuestionnaire.setFormType("H");
+    newQuestionnaire.setFormType(caseType.contentEquals("CE") ? "CE" : "H");
     newQuestionnaire.setQuestionnaireType("1");
 
     return ResponseEntity.ok(newQuestionnaire);
