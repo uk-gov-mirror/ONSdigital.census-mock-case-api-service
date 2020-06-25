faa94b0d2a540bac29a2118ae1437f059020c64f
Rob Catling <github@rpc-soft.com>
2020-06-25 17:13:32 +0100

    CR-760 prevent loading invalid (non-Luhn) case-ref data, and quieten expected test exceptions

--- src/main/java/uk/gov/ons/ctp/integration/mockcaseapiservice/utility/FailureSimulator.java
+++ src/main/java/uk/gov/ons/ctp/integration/mockcaseapiservice/utility/FailureSimulator.java
@@ -2,26 +2,25 @@ package uk.gov.ons.ctp.integration.mockcaseapiservice.utility;
 
 import java.util.HashMap;
 import java.util.Map;
-import uk.gov.ons.ctp.common.error.CTPException;
-import uk.gov.ons.ctp.common.error.CTPException.Fault;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public class FailureSimulator {
 
-  private static final Map<Integer, Fault> faultCodes = new HashMap<>();
+  private static final Map<Integer, HttpStatus> faultCodes = new HashMap<>();
 
   static {
-    faultCodes.put(400, Fault.BAD_REQUEST);
-    faultCodes.put(401, Fault.ACCESS_DENIED);
-    faultCodes.put(404, Fault.RESOURCE_NOT_FOUND);
-    faultCodes.put(500, Fault.SYSTEM_ERROR);
+    faultCodes.put(400, HttpStatus.BAD_REQUEST);
+    faultCodes.put(401, HttpStatus.UNAUTHORIZED);
+    faultCodes.put(404, HttpStatus.NOT_FOUND);
+    faultCodes.put(500, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
-  public static void optionallyTriggerFailure(String key, Integer... failureCodes)
-      throws CTPException {
+  public static void optionallyTriggerFailure(String key, Integer... failureCodes) {
     for (int failureCode : failureCodes) {
       String failureCodeAsString = Integer.toString(failureCode);
       if (key.startsWith(failureCodeAsString)) {
-        throw new CTPException(faultCodes.get(failureCode));
+        throw new ResponseStatusException(faultCodes.get(failureCode));
       }
     }
   }
